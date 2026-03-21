9f8532b223c07b5a24effebd10c705ccfa325a39
MINJI KIM <minzzzy1210@gmail.com>
2026-03-21 15:11:49 +0900

    [STEP 3] 10952

---
+++ 3단계/10952.js
@@ -0,0 +1,18 @@
+const cases = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const solution = (cases) => {
+  const result = [];
+  for (let i = 0; i < cases.length - 1; i++) {
+    const [a, b] = cases[i].split(' ').map(Number);
+
+    result.push(a + b);
+  }
+
+  return result.join('\n');
+};
+
+console.log(solution(cases));
